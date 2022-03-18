a13332623b6e2e565ca98d5109faeb9ae82537a0
Ziad Azab <100707085+Azab18@users.noreply.github.com>
2022-03-18 16:15:15 +0300

    done

--- ios-cw-5/ios-cw-5/ContentView.swift
+++ ios-cw-5/ios-cw-5/ContentView.swift
@@ -9,11 +9,49 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        VStack{
+        
+        HStack{
+        
+        Image("a")
+                .resizable()
+                .frame(width: 120, height: 140)
+                .clipShape(Circle())
+                .scaledToFit()
+        
+            Text("ios_memes")
+            .font(.title)
             .padding()
-    }
+        }
+            
+            HStack{
+            Image("1")
+                .resizable()
+                .frame(width: 140, height: 160)
+                Image("3")
+                    .resizable()
+                    .frame(width: 140, height: 160)
+               Image("2")
+                .resizable()
+                .frame(width: 140, height: 160)
+            }
+                HStack{
+                
+                Image("4")
+                    .resizable()
+                    .frame(width: 140, height: 160)
+                Image("5")
+                    .resizable()
+                    .frame(width: 140, height: 160)
+                Image("6")
+                    .resizable()
+                    .frame(width: 140, height: 160)
+                
+                }
+            
+            }
 }
-
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
